925b5fbd631a3b2c73ca82a1f72e65debf60fcc6
Wagner Correia <wagner@dextak.com.br>
2023-03-11 20:04:23 -0300

    Finalizado led mqtt

--- Led_MQTT.ino
+++ Led_MQTT.ino
@@ -1,3 +1,7 @@
+//Bibliotecas
+#include "ArduinoJson.h"
+#include "EspMQTTClient.h"
+
 //Declaração de variáveis
 int ledStatus = 2;
 int led = 22;
@@ -7,6 +11,66 @@ int estadoLed = 0;
 // variáveis para o botão:
 int ultimoEstado = 1;  //o estado anterior do pino de entrada
 int estadoAtual;       //a leitura atual do pino de entrada
+
+//-------------------------------------------------//
+//configurações da conexão MQTT
+EspMQTTClient client
+(
+  "wagner_rede", //nome da sua rede Wi-Fi
+  "13041968", //senha da sua rede Wi-Fi
+  "test.mosquitto.org",  // MQTT Broker server ip padrão da tago
+  "",   // username
+  "",   // Código do Token DA PLATAFORMA TAGOIO
+  "senaiesp001",     // Client name that uniquely identify your device
+  1883              // The MQTT port, default to 1883. this line can be omitted
+);
+
+//Tópico
+char topicoMqtt[] = "senai/esp32/001";
+
+/*
+Esta função é chamada assim que tudo estiver conectado (Wifi e MQTT)]
+AVISO: VOCÊ DEVE IMPLEMENTÁ-LO SE USAR EspMQTTClient
+*/ 
+void onConnectionEstablished()
+{
+    client.subscribe(topicoMqtt,onMessageReceived);
+}
+
+void onMessageReceived(const String &msg) {
+    digitalWrite(ledStatus, LOW);
+    Serial.println("Mensagem recebida:");
+    Serial.println(msg);
+
+    StaticJsonBuffer<300> JSONBuffer;                         //Memory pool
+    JsonObject& parsed = JSONBuffer.parseObject(msg); //Parse message
+
+    if (parsed.success()) {
+      int ledMqtt = parsed["led"];
+
+      if (ledMqtt == 1) {
+        ligarLed();
+        Serial.println("Led ligado por MQTT");
+      } else if (ledMqtt == 2) {
+        desligarLed();
+        Serial.println("Led desligado por MQTT");
+      }
+
+    } else {
+      Serial.println("Falha ao realizar parsing do JSON");
+    }
+}
+
+void transmitirStatusLed() {
+  StaticJsonBuffer<300> JSONbuffer;
+  JsonObject& JSONencoder = JSONbuffer.createObject();
+  JSONencoder["statusLed"] = estadoLed;
+
+  char JSONmessageBuffer[300];
+  JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+
+  client.publish(topicoMqtt, JSONmessageBuffer);
+}
 
 void setup() {
 
@@ -19,7 +83,7 @@ void setup() {
 
   //Informação de Placa ligada
   pinMode(ledStatus, OUTPUT);
-  digitalWrite(ledStatus, HIGH);
+  digitalWrite(ledStatus, LOW);
 }
 
 void alternarLed() {
@@ -27,11 +91,27 @@ void alternarLed() {
     //Liga o led
     estadoLed = 1;
     digitalWrite(led, HIGH);
+    Serial.println("Led ligado por botão");
   } else {
     //Desliga o led
     estadoLed = 0;
     digitalWrite(led, LOW);
+    Serial.println("Led desligado por botão");
   }
+
+  transmitirStatusLed();
+}
+
+void ligarLed() {
+  estadoLed = 1;
+  digitalWrite(led, HIGH);
+  transmitirStatusLed();
+}
+
+void desligarLed() {
+  estadoLed = 0;
+  digitalWrite(led, LOW);
+  transmitirStatusLed();
 }
 
 void loop() {
@@ -46,5 +126,14 @@ void loop() {
   }
 
   ultimoEstado = estadoAtual;
-  delay(50);
+
+  //Alternar o led "azul" do ESP32 conforme estado na conexão
+  if (client.isWifiConnected()) {
+    digitalWrite(ledStatus, HIGH);
+  } else {
+    digitalWrite(ledStatus, LOW);
+  }
+
+  client.loop();
+  delay(10);
 }
